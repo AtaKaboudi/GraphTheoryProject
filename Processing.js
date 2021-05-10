c3b284c73507140f8ea1fd526be557884f9a5283
AtaKaboudi <ata.kaboudi@gmail.com>
2021-05-11 00:58:23 +0100

    venuesAPI

--- Processing.js
+++ Processing.js
@@ -1,63 +1,37 @@
-import {venue} from './venue.js';
-
+const output = document.getElementById('output')
 const basePath = '../assets/csv'
+const files = ['/paris-accommodation.csv','/paris-attraction.csv','/paris-poi.csv','/paris-restaurant.csv'];
+export const venuesList = [];
 
-function  fetchCSV (path){
+
+function fetchCSV(path){
     return fetch (basePath + path)
         .then(res => res.text())
         .then((text)=>{
-            return text;
+
+            let arr = text.split("\n");
+            let valid = [];
+            for(let a of arr){
+               let  params=  a.split(",");
+                if(params.length == 11){
+                    valid.push(a);
+                }
+            }
+            return valid;
+
         })
         .catch(error=>console.log(error));
+    }
+
+
+ export async function populateVenues(){ 
+      for(let f of files){
+        fetchCSV(f).then((res) =>  {
+          
+              venuesList.push(res);
+            
+        });
+    }
 }
-
-function getColumnsIndex (columns){
-    return [columns.indexOf("id")
-    ,columns.indexOf("address")
-    ,columns.indexOf('category')
-    ,columns.indexOf('lat')
-    ,columns.indexOf('lng')
-    ,columns.indexOf('location')
-    ,columns.indexOf('name')
-    ,columns.indexOf('originalId')
-    ,columns.indexOf('polarity')
-    ,columns.indexOf('subCategory')
-    ,columns.indexOf('details')
-    ,columns.indexOf('reviews')
-]    
-}
-function populateVenues(textArray,columns){
-    let venuesList = [];
-    textArray.shift();
-    textArray.forEach(element => {
-    let params = element.split(",");
-    let aux = new venue(params[columns[0]],
-        params[columns[1]],
-        params[columns[2]],
-        params[columns[3]],
-        params[columns[4]],
-        params[columns[5]],
-        params[columns[6]],
-        params[columns[7]],
-        params[columns[8]],
-        params[columns[9]],
-        params[columns[10]],
-        params[columns[11]],
-        params[columns[12]],
-    )
-    venuesList.push(aux);
-});
-return venuesList;
-}
-
-export function parseCSV(fileName){
-return fetchCSV(fileName).then((text)=>{
-    let columns = getColumnsIndex(text.split("\n")[0].split(","));
-    return( populateVenues(text.split("\n"),columns));
-});
-
-
-}
-
 
 
--- script.js
+++ script.js
@@ -1,17 +1,55 @@
- import {parseCSV} from './Processing.js'
-var venuesList = [];
-var categories = [];
-var output = document.querySelector('#output');
-async function  populateVenues(){
- await parseCSV('/paris-restaurant.csv').then(res => venuesList = venuesList.concat(res))
- await parseCSV('/paris-accommodation.csv').then(res => venuesList =venuesList.concat(res))
- await parseCSV('/paris-poi.csv').then(res => venuesList =venuesList.concat(res))
- await  parseCSV('/paris-attraction.csv').then((res) => {venuesList =venuesList.concat(res) })
- let char = "";
-    for( let e of venuesList){
-        char = char + e.review +" ";
+import {populateVenues, venuesList} from './Processing.js'
+var output = document.getElementById('output')
+
+// wait to  populate venues in List 
+ function switchIndex(category){
+    let index ;
+    switch(category){
+        case "restaurant":
+            index = 3 ;
+            break; 
+        case "poi":
+            index = 2 ;
+            break; 
+        case "attraction":
+            index = 1 ;
+            break; 
+        case "accommodation":
+            index = 0 ;
+            break;
+        }
+        return index ;
+ }
+export const venueAPI = {
+    populateVenues(){
+        populateVenues().then(()=>console.log("a"));
     }
-    output.textContent = char;
+    ,
+    getrandom(category,limit)  {
+        let index  = switchIndex(category) ;
+        if(!index) return ["Wrong category"]
+        let result = [];
+        for( let i= 0 ; i < limit ;i++){
+            result = result.concat(venuesList[index][1000]);
+        }
+        return result
+    }
+    ,
+    getName(category,keyWord){
+        let result = []
+        let index  = switchIndex(category) ;
+        if ((!index) || (!keyWord)) return ["Wrong Arguments"]
+
+        for(let v of venuesList[index]){
+           let name = v.split(",")[1];
+            if(name.includes(" " +keyWord+ " ")){
+                result = result.concat(v);
+            }
+        }
+        return result;
+    }
+
 }
 
-populateVenues();
+venueAPI.populateVenues();
+setTimeout(()=> {console.log(venueAPI.getName("restaurant","italien"))},3000);
